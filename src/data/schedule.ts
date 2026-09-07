0d2b880885f28a0905f1f11a3661461406caf3f0
bcjacha-dot <bcjacha@gmail.com>
2026-09-08 01:23:02 +0200

    Delete src directory

--- src/data/schedule.ts
+++
@@ -1,7 +0,0 @@
-import { RaceSchedule } from '@/types';
-
-export const schedule: RaceSchedule[] = [
-  { id: 's1', date: '2026-03-08', trackName: 'Jeddah Corniche Circuit', category: 'F1' },
-  { id: 's2', date: '2026-03-15', trackName: 'Losail International Circuit', category: 'MotoGP' },
-  // ... total 5 races
-];
